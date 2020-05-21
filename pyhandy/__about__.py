9805ce7bb7f5b5eb4bf3ee1958cac7cc43745b04
Toby Qin <toby.qin@live.com>
2020-05-21 22:59:11 +0800

    init package structure.

---
+++ pyhandy/__about__.py
@@ -0,0 +1,8 @@
+__package_name__ = 'pyhandy'
+__version__ = '0.0.1'
+__author__ = 'Toby Qin'
+__author_email__ = 'toby.qin@live.com'
+__description__ = (
+    'Pyhandy is a module with some convenient utilities not included '
+    'with the standard Python install')
+__url__ = 'https://github.com/tobyqin/pyhandy'
