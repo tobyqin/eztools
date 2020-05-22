6a99af1c233ae8fff120e414b24132e64ecaf999
Toby Qin <toby.qin@live.com>
2020-05-22 10:14:01 +0800

    rename to eztools.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# pyhandy
-My handy module for Python programming.
+# eztools
+
+My handy module for Python programming, to make my life easier.
+
+> eztools => easy tool collection.
+
+## Installation
+
+```bash
+pip install eztools -U
+```
+
+## License
+
+MIT
--- setup.py
+++ setup.py
@@ -6,30 +6,30 @@ import setuptools
 # To prevent importing about and thereby breaking the coverage info we use this
 # exec hack
 about = {}
-with open('python_utils/__about__.py') as fp:
+with open('eztools/__about__.py') as fp:
     exec(fp.read(), about)
 
 if os.path.isfile('README.md'):
     long_description = open('README.md').read()
 else:
-    long_description = 'See http://pypi.python.org/pypi/pyhandy/'
+    long_description = 'See http://pypi.python.org/pypi/eztools/'
 
 needs_pytest = set(['ptr', 'pytest', 'test']).intersection(sys.argv)
 pytest_runner = ['pytest-runner'] if needs_pytest else []
 
 if __name__ == '__main__':
     setuptools.setup(
-        name='pyhandy',
+        name='eztools',
         version=about['__version__'],
         author=about['__author__'],
         author_email=about['__author_email__'],
         description=about['__description__'],
         url=about['__url__'],
-        license='BSD',
+        license='MIT',
         packages=setuptools.find_packages(),
         long_description=long_description,
-        install_requires=['six'],
+        install_requires=['requests', 'arrow'],
         tests_require=['pytest'],
         setup_requires=[] + pytest_runner,
-        classifiers=['License :: OSI Approved :: BSD License'],
+        classifiers=['License :: OSI Approved :: MIT License'],
     )
